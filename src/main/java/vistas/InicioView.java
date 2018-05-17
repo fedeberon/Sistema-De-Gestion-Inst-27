d6b7a11fcf2a5ea600932062c598e54b0a7a69ef
enzomunoz <enzo.munooz@hotmail.com>
2018-05-17 20:40:49 -0300

    Se creo la ventana y se declararon componentes

--- src/main/java/vistas/InicioView.java
+++ src/main/java/vistas/InicioView.java
@@ -1,8 +1,16 @@
 package vistas;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -16,33 +24,74 @@ public class InicioView extends Application {
             //Creating a Text object
             Text text = new Text();
 
-            //Setting the text to be added.
-            text.setText("Hello how are you");
+            //Titulo de  la ventana
+            text.setText("Sistema De Gestion Instituto 27");
 
-            //setting the position of the text
-            text.setX(50);
+            //Posicion del titulo
+            text.setX(500);
             text.setY(50);
+
+
             text.setStyle("-fx-color: yellow;");
+            //Text edit
+            text.setFont(new Font("Lucida Grande",24));
+            text.setFill(Color.BLACK);
+
             //Creating a Group object
             Group root = new Group(text);
 
             //Creating a scene object
-            Scene scene = new Scene(root, 600, 300);
+            Scene scene = new Scene(root, 1280, 768);
             scene.getStylesheets().add("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css");
 
-            Text titulo = new Text("Sample Application");
-
-            titulo.setStyle("-fx-height: 200px");
-
             //Setting title to the Stage
-            stage.setTitle("Sample Application");
+            stage.setTitle("Instituto 27");
 
             //Adding scene to the stage
             stage.setScene(scene);
 
             //Displaying the contents of the stage
             stage.show();
+
+            //Declaracion de objetos
+            Button btnAlu = new Button();
+            Button btnPro = new Button();
+            Button btnMat = new Button();
+            Button btnCar = new Button();
+
+            btnAlu.setText("Alumnos");
+
+            //Añadir elemento a la ventana (this.add en java swing)
+            root.getChildren().add(btnAlu);
+                      
+
+
+
+            btnAlu.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+
+                }
+            });
+
+
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         public static void main(String args[]){
             launch(args);
         }
