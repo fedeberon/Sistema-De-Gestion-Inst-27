12a06e59c2cdddf40c53018b4c1a584928b76820
fedeberon <federicoberon@outlook.com>
2018-06-06 20:27:43 -0300

    Se elimino libreria que no se usaba.

--- src/main/java/controller/DrapoDashboard.java
+++ src/main/java/controller/DrapoDashboard.java
@@ -5,7 +5,6 @@
  */
 package controller;
 
-import com.sun.deploy.uitoolkit.impl.fx.AppletStageManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
