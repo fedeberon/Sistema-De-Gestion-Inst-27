e4705d2b5c6046126ef07e6fd909ef41ee751bfb
marianaleguizamon <mariana.agus93@gmail.com>
2018-10-26 20:47:05 -0300

    Modificado el HomeController para que detecte la interfaz de profesor

--- src/main/java/com/instituto27/controller/HomeController.java
+++ src/main/java/com/instituto27/controller/HomeController.java
@@ -70,7 +70,7 @@ public class HomeController implements Initializable {
         pnl_scroll.getChildren().clear();
         Node [] nodes = new  Node[2];
         Integer v = 1;
-        nodes[v] = (Node)FXMLLoader.load(getClass().getResource("/fxml/menu/Item.fxml"));
+        nodes[v] = (Node)FXMLLoader.load(getClass().getResource("/fxml/profesor/Profesor.fxml"));
         pnl_scroll.getChildren().add(nodes[v]);
     }
 
